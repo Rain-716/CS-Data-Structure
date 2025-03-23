fa551ac40dbd03b5c1b11bac155540d41f4a763e
Rain S <10245102440@stu.ecnu.edu.cn>
2025-03-23 16:52:34 +0800

    Add files via upload

---
+++ Statement Compression.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int maxLength (string& s)
+{
+    vector <int> next(s.length()+1);
+    next[0]=-1;next[1]=0;
+    for (int i=2;i<=s.length();i++){
+        int k=next[i-1];
+        while (k>=0&&s[i-1]!=s[k]){
+            k=next[k];
+        }
+        next[i]=k+1;
+    }
+    return next.back();
+}
+int main()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    if (n==1){
+        cout<<s<<endl;
+        return 0;
+    }
+    for (int i=1;i<n;i++){
+        string t;
+        cin>>t;
+        int minL=min(s.length(),t.length());
+        string ts=t.substr(0,minL)+" "+s.substr(s.length()-minL,minL);
+        s+=t.substr(maxLength(ts));
+    }
+    cout<<s<<endl;
+    return 0;
+}
